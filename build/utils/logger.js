78fbfbb8279ab2091bdb710210b46aa0fb11248b
miaowing <i@zfeng.net>
2018-04-12 14:31:05 +0800

    update logger

--- build/utils/logger.js
+++ build/utils/logger.js
@@ -12,9 +12,11 @@ var _path = require('path');
 
 var _path2 = _interopRequireDefault(_path);
 
-var _winston = require('winston');
+var _moment = require('moment');
 
-var _winston2 = _interopRequireDefault(_winston);
+var _moment2 = _interopRequireDefault(_moment);
+
+var _winston = require('winston');
 
 var _bootstrap = require('../config/bootstrap');
 
@@ -28,22 +30,28 @@ function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj;
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+const { combine, timestamp, label, printf } = _winston.format;
 const logPath = bootstrap.getConfig('logger.path', __dirname);
 
 if (!_fs2.default.existsSync(logPath)) {
     _mkdirp2.default.sync(logPath);
 }
 
-exports.default = new _winston2.default.Logger({
+const myFormat = printf(info => {
+    return `[${info.label}] [${(0, _moment2.default)(info.timestamp).format('YYYY-MM-DD h:mm:ss')}] [${info.level}]: ${info.message}`;
+});
+
+exports.default = (0, _winston.createLogger)({
     level: process.env.NODE_ENV === 'production' ? 'info' : 'debug',
-    transports: [new _winston2.default.transports.Console({
+    format: combine(_winston.format.colorize(), label({ label: bootstrap.getConfig('web.serviceName') }), timestamp(), myFormat),
+    transports: [new _winston.transports.Console({
         colorize: true
-    }), new _winston2.default.transports.File({
+    }), new _winston.transports.File({
         name: 'info-file',
         filename: _path2.default.resolve(logPath, 'log-info.log'),
         maxsize: 100 * 1024 * 1024,
         level: 'info'
-    }), new _winston2.default.transports.File({
+    }), new _winston.transports.File({
         name: 'error-file',
         filename: _path2.default.resolve(logPath, 'log-error.log'),
         maxsize: 100 * 1024 * 1024,
--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,28 +1,40 @@
 import fs from 'fs';
 import path from 'path';
-import winston from 'winston';
+import moment from 'moment';
+import {createLogger, transports, format} from 'winston';
 import * as bootstrap from '../config/bootstrap';
 import mkdirp from 'mkdirp';
 
+const {combine, timestamp, label, printf} = format;
 const logPath = bootstrap.getConfig('logger.path', __dirname);
 
 if (!fs.existsSync(logPath)) {
     mkdirp.sync(logPath);
 }
 
-export default new winston.Logger({
+const myFormat = printf(info => {
+    return `[${info.label}] [${moment(info.timestamp).format('YYYY-MM-DD h:mm:ss')}] [${info.level}]: ${info.message}`;
+});
+
+export default createLogger({
     level: process.env.NODE_ENV === 'production' ? 'info' : 'debug',
+    format: combine(
+        format.colorize(),
+        label({label: bootstrap.getConfig('web.serviceName')}),
+        timestamp(),
+        myFormat
+    ),
     transports: [
-        new winston.transports.Console({
+        new transports.Console({
             colorize: true
         }),
-        new winston.transports.File({
+        new transports.File({
             name: 'info-file',
             filename: path.resolve(logPath, 'log-info.log'),
             maxsize: 100 * 1024 * 1024,
             level: 'info'
         }),
-        new winston.transports.File({
+        new transports.File({
             name: 'error-file',
             filename: path.resolve(logPath, 'log-error.log'),
             maxsize: 100 * 1024 * 1024,
