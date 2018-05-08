5351c5e25e5b093981993b0b6c514ca8defdb98c
miaowing <i@zfeng.net>
2018-05-08 16:11:03 +0800

    compiled

--- build/utils/logger.js
+++ build/utils/logger.js
@@ -18,6 +18,8 @@ var _moment2 = _interopRequireDefault(_moment);
 
 var _winston = require('winston');
 
+var _winston2 = _interopRequireDefault(_winston);
+
 var _bootstrap = require('../config/bootstrap');
 
 var bootstrap = _interopRequireWildcard(_bootstrap);
@@ -30,31 +32,36 @@ function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj;
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-const { combine, timestamp, label, printf } = _winston.format;
 const logPath = bootstrap.getConfig('logger.path', __dirname);
 
 if (!_fs2.default.existsSync(logPath)) {
     _mkdirp2.default.sync(logPath);
 }
 
-const myFormat = printf(info => {
-    return `[${info.label}] [${(0, _moment2.default)(info.timestamp).format('YYYY-MM-DD h:mm:ss')}] [${info.level}]: ${info.message}`;
-});
-
-exports.default = (0, _winston.createLogger)({
+exports.default = new _winston2.default.Logger({
     level: process.env.NODE_ENV === 'production' ? 'info' : 'debug',
-    format: combine(_winston.format.colorize(), label({ label: bootstrap.getConfig('web.serviceName') }), timestamp(), myFormat),
-    transports: [new _winston.transports.Console({
-        colorize: true
-    }), new _winston.transports.File({
+    transports: [new _winston2.default.transports.Console({
+        colorize: true,
+        label: bootstrap.getConfig('web.serviceName'),
+        timestamp: () => {
+            return (0, _moment2.default)(new Date().getTime()).format('YYYY-MM-DD h:mm:ss');
+        }
+    }), new _winston2.default.transports.File({
         name: 'info-file',
-        filename: _path2.default.resolve(logPath, 'log-info.log'),
+        filename: _path2.default.resolve(logPath, 'log.log'),
         maxsize: 100 * 1024 * 1024,
-        level: 'info'
-    }), new _winston.transports.File({
+        label: bootstrap.get('web.serviceName'),
+        timestamp: () => {
+            return (0, _moment2.default)(new Date().getTime()).format('YYYY-MM-DD h:mm:ss');
+        }
+    }), new _winston2.default.transports.File({
         name: 'error-file',
         filename: _path2.default.resolve(logPath, 'log-error.log'),
         maxsize: 100 * 1024 * 1024,
-        level: 'error'
+        level: 'error',
+        label: bootstrap.get('web.serviceName'),
+        timestamp: () => {
+            return (0, _moment2.default)(new Date().getTime()).format('YYYY-MM-DD h:mm:ss');
+        }
     })]
 });
